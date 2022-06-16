c241e86dc5755c8ba271a335051237ac82d225d9
Stephanie Tahova <106015292+sstephanie7@users.noreply.github.com>
2022-06-16 17:30:26 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _08._Math_Power
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double groundNumber = double.Parse(Console.ReadLine());
+            double power = double.Parse(Console.ReadLine());
+
+            Console.WriteLine(MathPower(groundNumber, power));
+        }
+        static double MathPower(double groundNumber, double power)
+        {
+            double result = 0;
+
+            result = Math.Pow(groundNumber, power);
+
+            return result;
+        }
+    }
+}
